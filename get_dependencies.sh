a784f2d38939eebcb73b3da65ca3f56f48ccc583
Bill Allen <photo.allen@gmail.com>
2019-10-20 14:45:56 -0600

    More links to dependencies.

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 wget -O Adafruit_Sensor-1.0.3.zip https://github.com/adafruit/Adafruit_Sensor/archive/1.0.3.zip
+wget -O Adafruit_ADXL343-1.1.0.zip https://github.com/adafruit/Adafruit_ADXL343/archive/1.1.0.zip  # motion sensor
+wget -O Adafruit_ADT7410-1.0.1.zip https://github.com/adafruit/Adafruit_ADT7410/archive/1.0.1.zip  # temperature sensor
+wget -O Adafruit_GPS-1.2.0.zip https://github.com/adafruit/Adafruit_GPS/archive/1.2.0.zip
