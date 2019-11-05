1d3d3d697c1f1c23397a9af352abe94850da5b0c
Bill Allen <photo.allen@gmail.com>
2019-11-04 20:34:40 -0700

    Added in RFM95W radio code.

--- feathersat/feathersat.ino
+++ feathersat/feathersat.ino
@@ -1,3 +1,27 @@
+//
+// for RFM9x radio
+//
+#include <SPI.h>
+#include <RH_RF95.h>
+
+// for feather m0 version 
+//#define RFM95_CS 8
+//#define RFM95_RST 4
+//#define RFM95_INT 3
+ 
+// for shield version
+#define RFM95_CS 10
+#define RFM95_RST 9
+#define RFM95_INT 7
+
+#define RF95_FREQ 434.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+//
+// for GPS
+//
 #include <Adafruit_GPS.h>
 
 // what's the name of the hardware serial port?
@@ -8,12 +32,47 @@ Adafruit_GPS GPS(&GPSSerial);
 
 uint32_t timer = millis();
 
+void reset_radio()
+{
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+}
+
+void setup_radio()
+{
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+  reset_radio();
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    while (1);
+  }
+
+  Serial.println("LoRa radio init OK!");
+ 
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+
+  Serial.print("Set Freq to: ");
+  Serial.println(RF95_FREQ);
+
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+ 
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+}
+
 void setup_gps()
 {
-  // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
-  // also spit it out
-  Serial.begin(115200);
-
   // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
   GPS.begin(9600);
 
@@ -36,7 +95,14 @@ void setup_gps()
 
 void setup()
 {
+  Serial.begin(115200);
+  setup_radio();
   setup_gps();
+}
+
+void transmit_message(char *message, int message_length)
+{
+  rf95.send((uint8_t *)message, message_length);
 }
 
 void update_gps_data()
